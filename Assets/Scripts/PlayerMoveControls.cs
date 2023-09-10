3a27a350916febe2508204567e1c698aab0f9609
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-10 10:28:39 +0300

    flip the player

--- Assets/Scripts/PlayerMoveControls.cs
+++ Assets/Scripts/PlayerMoveControls.cs
@@ -8,21 +8,38 @@ public class PlayerMoveControls : MonoBehaviour
 
     private GatherInput gI;
     private Rigidbody2D rb;
-    
+
+    private int direction = 1;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         gI = GetComponent<GatherInput>();
     }
 
-    
+
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        Flip();
         rb.velocity = new(speed * gI.valueX, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        if (gI.valueX * direction < 0)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
+            direction *= -1;
+        }
     }
 }
