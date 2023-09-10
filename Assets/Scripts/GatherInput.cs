abb62776821523f2b6abe17d2217e9fae3bf883f
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-10 10:15:22 +0300

    when release key inputs will be cleared now (fix player movement)

--- Assets/Scripts/GatherInput.cs
+++ Assets/Scripts/GatherInput.cs
@@ -19,12 +19,26 @@ public class GatherInput : MonoBehaviour
     private void OnEnable()
     {
         myControls.Player.Move.performed += StartMove;
+        myControls.Player.Move.canceled += StopMove;
 
         myControls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        myControls.Player.Move.performed -= StartMove;
+        myControls.Player.Move.canceled -= StopMove;
+
+        myControls.Player.Disable();
     }
 
     private void StartMove(InputAction.CallbackContext ctx)
     {
         valueX = ctx.ReadValue<float>();
+    }
+
+    private void StopMove(InputAction.CallbackContext ctx)
+    {
+        valueX = 0f;
     }
 }
