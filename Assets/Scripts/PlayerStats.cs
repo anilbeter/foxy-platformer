36480d4f29c39817db046e7854a11d73c91a0a40
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-11 15:59:00 +0300

    setup PlayerStats script, add TakeDamage method and condition about if player dead

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
+    public float maxHealth;
+    public float health;
 
     void Start()
     {
-
+        health = maxHealth;
     }
 
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        // play hurt animation
 
+        if (health <= 0)
+        {
+            GetComponent<PolygonCollider2D>().enabled = false;
+            Debug.Log("Player is dead :(");
+        }
+    }
 }
