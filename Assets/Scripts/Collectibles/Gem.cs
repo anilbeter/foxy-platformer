baded23030f7fe9992c54d22e9097ab173de568e
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-11 09:56:22 +0300

    add collect the gems feature

--- Assets/Scripts/Collectibles/Gem.cs
+++ Assets/Scripts/Collectibles/Gem.cs
@@ -10,9 +10,18 @@ public class Gem : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+        // NOTE: Is collision touching/triggering the player? In this situation collision is Gem, because this script attached to the Gem object 
+        if (collision.CompareTag("Player"))
+        {
+            // gameObject -> Gem
+            // Destroy(gameObject);
 
+            // [ALTERNATIVE WAY] Destroying game object may be problem in the future for implement sounds, so better way:
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<CircleCollider2D>().enabled = false;
+            // In real world project, you should assign GetComponent in Start or Awake functions. I just quickly get them without storing, this is more expansive PC performance but whatever its just 2d platformer and I won't publish it on steam, so anyway
+        }
     }
 }
