545716feb1165b70fb613428ca52cef0baae039b
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-11 16:38:18 +0300

    using Coroutines for damage prevention time

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -7,6 +7,8 @@ public class PlayerStats : MonoBehaviour
     public float maxHealth;
     public float health;
 
+    public bool canTakeDamage = true;
+
     void Start()
     {
         health = maxHealth;
@@ -14,14 +16,37 @@ public class PlayerStats : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
-        health -= damage;
-        // play hurt animation
-
-        if (health <= 0)
+        if (canTakeDamage)
         {
-            GetComponent<PolygonCollider2D>().enabled = false;
-            GetComponentInParent<GatherInput>().DisableControls();
-            Debug.Log("Player is dead :(");
+            health -= damage;
+            // play hurt animation
+
+            if (health <= 0)
+            {
+                GetComponent<PolygonCollider2D>().enabled = false;
+                GetComponentInParent<GatherInput>().DisableControls();
+                Debug.Log("Player is dead :(");
+            }
+
+            StartCoroutine(DamagePrevention());
+        }
+    }
+
+    // Idea -> I want to delay/wait after take damage until take next damage. I should use Coroutines for add delay
+    private IEnumerator DamagePrevention()
+    {
+        canTakeDamage = false;
+        // When player takes damage, will wait 0.15 seconds to take next damage 
+        yield return new WaitForSeconds(0.15f);
+        // Player is still alive?
+        if (health > 0)
+        {
+            // after 0.15 seconds later, again player can take damage.
+            canTakeDamage = true;
+        }
+        else
+        {
+            // play death animation
         }
     }
 }
