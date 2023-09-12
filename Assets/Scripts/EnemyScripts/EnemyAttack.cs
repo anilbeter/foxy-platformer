888475395583f25b71b2948fb13dba38ef6e86a9
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-12 11:23:11 +0300

    setup EnemyAttack script for feature that player takes damage when touch the enemy

--- Assets/Scripts/EnemyScripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyScripts/EnemyAttack.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float damage;
+    protected PlayerStats playerStats;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Stats"))
+        {
+            playerStats = collision.GetComponent<PlayerStats>();
+            playerStats.TakeDamage(damage);
+        }
     }
 }
