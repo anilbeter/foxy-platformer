a40bcdca544317cb47dcadef0adb62815f4d6d91
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-12 09:58:07 +0300

    add note about inherit variables (protected)

--- Assets/Scripts/EnemyScripts/Enemy.cs
+++ Assets/Scripts/EnemyScripts/Enemy.cs
@@ -6,6 +6,10 @@ public class Enemy : MonoBehaviour
 {
     public float health;
     // PatrollingEnemy.cs can use this variable, because it's child of Enemy.cs. I don't even need to connect them like "private Enemy enemy;" , "GetComponent<Enemy>();". I DONT need those codes!!!
+    // NOTE: inherit variables must be public or protected
+    // protected -> 1) I can't see them in inspector (just like private variables)
+    // 2) other classes (except child classes) neither cant see nor use protected variables
+    // 3) visible just to parent and child classes
 
     void Start()
     {
