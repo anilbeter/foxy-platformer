d9ffe024598f8f7dfa3d128a3f6537297418eb1c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-12 15:01:02 +0300

    [setup Door script] [SceneManager] now game level changes to level2 when touch door

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,11 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
+    public int lvlToLoad;
 
     void Start()
     {
 
     }
 
+    private void LoadLevel()
+    {
+        SceneManager.LoadScene(lvlToLoad);
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            // To ensure dont call load level function multiple times, I disable it
+            GetComponent<BoxCollider2D>().enabled = false;
+            // also I want to disable player controls
+            collision.GetComponent<GatherInput>().DisableControls();
+
+            LoadLevel();
+        }
+    }
 }
