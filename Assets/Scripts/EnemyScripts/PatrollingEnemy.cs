ae4add0cb6918bdf8ff3f99b234857fe1349be49
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-12 09:50:26 +0300

    make Enemy.cs parent class, PatrollingEnemy.cs child class

--- Assets/Scripts/EnemyScripts/PatrollingEnemy.cs
+++ Assets/Scripts/EnemyScripts/PatrollingEnemy.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PatrollingEnemy : MonoBehaviour
+public class PatrollingEnemy : Enemy
+// Now Enemy.cs is parent class, PatrollingEnemy.cs is child class
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
