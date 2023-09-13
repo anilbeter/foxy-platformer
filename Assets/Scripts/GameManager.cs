091ad6e4ee53e661333477680e2261a541a2983c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-13 09:04:15 +0300

    add destory second GameManager condition with comm

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,8 +14,18 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        // Keep this GameManager object and prevent destroying it when new scene/level loaded
-        DontDestroyOnLoad(gameObject);
+        if (GM == null)
+        {
+            // this means -> this whole GameManager script
+            GM = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        // Idea -> REMEMBER: there's must only one game manager on each level. Player just started the game and there's not GM atm, so if condition will work (line 20 and 21). Then player arrived to level 2, oh no, there's two game manager object one of them from level 1 (cuz' in the if statement we use DontDestroyOnLoad), so now we have 2 game manager object in level 2 / scene 2. Now else statement works and Destroy previous one, again we have just 1 game manager, as it should be
+
     }
 
     void Start()
