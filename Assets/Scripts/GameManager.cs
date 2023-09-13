de2a13a73d094384682d2e2ae4a36f0398c69558
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-13 08:52:30 +0300

    add DontDestroyOnLoad function in GM

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,12 @@ public class GameManager : MonoBehaviour
     // I can control my whole important stuff like player, door, fade, etc. with GameManager. I don't need to connect them each other directly.
     // We need just one game manager in every scene. Idea is move this game manager from level to level
 
+    private void Awake()
+    {
+        // Keep this GameManager object and prevent destroying it when new scene/level loaded
+        DontDestroyOnLoad(gameObject);
+    }
+
     void Start()
     {
 
