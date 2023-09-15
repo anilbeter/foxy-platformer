46f50fc1b017779f32f1286fea03ffa3903a267f
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-15 11:43:03 +0300

    setup movingplatform script

--- Assets/Scripts/PlatformScript/MovingPlatform.cs
+++ Assets/Scripts/PlatformScript/MovingPlatform.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
   public float speed;
+  public Transform[] points;
+
+  private int i;
 
   void Start()
   {
 
   }
 
-  // Update is called once per frame
+
   void Update()
   {
+    if (UnityEngine.Vector2.Distance(transform.position, points[i].position) < 0.02f)
+    {
+      i++;
+      if (i == points.Length)
+      {
+        i = 0;
+      }
+    }
 
+    transform.position = UnityEngine.Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
   }
 }
