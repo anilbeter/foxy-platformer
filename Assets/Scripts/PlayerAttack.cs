227cf9e7336911a70692891429c911752e36db7f
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-18 11:58:57 +0300

    setup PlayerAttack script [now engine detects player attacking to enemy, oof!]

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float attackDamage;
+    private int enemyLayer;
+
     void Start()
     {
-        
+        enemyLayer = LayerMask.NameToLayer("Enemy");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.layer == enemyLayer)
+        {
+            Debug.Log("Hit enemy!");
+        }
     }
 }
